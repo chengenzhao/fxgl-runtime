9480a1a2a2fc37bebe1384ecd9107a3ef14406ac
chengenzhao <chengen.zhao@mail.mcgill.ca>
2021-12-23 16:28:45 +0800

    improve pom.xml format

--- src/main/java/com/whitewoodcity/Main.java
+++ src/main/java/com/whitewoodcity/Main.java
@@ -1,12 +1,15 @@
 package com.whitewoodcity;
 
 import javafx.application.Application;
-import javafx.scene.text.Font;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   @Override
   public void start(Stage stage) {
+    stage.setScene(new Scene(new Label("hello fxgl")));
+    stage.show();
   }
 
   public static void main(String[] args) {
