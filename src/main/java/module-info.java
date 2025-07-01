3f34ee5fface7f582af639b4fbc441a5d358511b
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-07-01 21:14:26 +0800

    the demo of showing javafx media bug project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,6 @@ open module com.whitewoocity.fxglruntime {
   requires com.almasb.fxgl.all;
   requires com.fasterxml.jackson.databind;
   requires com.whitewoodcity.fxcity;
+  requires javafx.media;
+  requires atlantafx.base;
 }
