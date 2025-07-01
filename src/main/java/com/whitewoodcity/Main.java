22dcc0beaa4269ab18a222f78019718e2ca3de2e
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-07-01 21:13:52 +0800

    the demo of showing javafx media bug project

--- src/main/java/com/whitewoodcity/Main.java
+++ src/main/java/com/whitewoodcity/Main.java
@@ -2,15 +2,12 @@ package com.whitewoodcity;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   @Override
   public void start(Stage stage) {
-    stage.setScene(new Scene(new Label("hello fxgl")));
-
-    var game = GameApp.embeddedLaunch(new GameApp());
+    stage.setScene(new Scene(GameApp.embeddedLaunch(new GameApp())));
 
     stage.show();
   }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,4 @@ open module com.whitewoocity.fxglruntime {
   requires com.almasb.fxgl.all;
   requires com.fasterxml.jackson.databind;
   requires com.whitewoodcity.fxcity;
-  requires javafx.media;
-  requires atlantafx.base;
 }
