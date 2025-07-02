b4ae43caad81554037c9acdd17145cbc4a9392ba
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-07-02 09:58:32 +0800

    releasing fxcity 0.5.5

--- src/main/java/com/whitewoodcity/GameApp.java
+++ src/main/java/com/whitewoodcity/GameApp.java
@@ -3,8 +3,10 @@ package com.whitewoodcity;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
-import javafx.animation.Transition;
-import javafx.util.Duration;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
 
 public class GameApp extends GameApplication {
 
@@ -12,19 +14,22 @@ public class GameApp extends GameApplication {
   protected void initSettings(GameSettings settings) {}
 
   @Override
-  protected void initGame() {
+  protected void initGame(){
     FXGL.getAssetLoader().clearCache();
 
-    var bgm = FXGL.loopBGM("maou_game_theme01.mp3");
-    bgm.getAudio().setVolume(0);
-    new Transition() {
-      {
-        setCycleDuration(Duration.seconds(1));
-      }
+    try {
 
-      protected void interpolate(double frac) {
-        bgm.getAudio().setVolume(FXGL.getSettings().getGlobalMusicVolume() * frac);
-      }
-    }.play();
+//      String path = GameApp.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+//      String decodedPath = URLDecoder.decode(path, "UTF-8");
+//      System.out.println(decodedPath);
+
+      var path = new File("").getAbsolutePath();
+
+      Media audio = new Media(new File(path+File.separator+"music"+File.separator+"maou_game_theme01.mp3").toURI().toURL().toExternalForm());
+      var mediaPlayer = new MediaPlayer(audio);
+      mediaPlayer.play();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
   }
 }
--- src/main/java/com/whitewoodcity/Main.java
+++ src/main/java/com/whitewoodcity/Main.java
@@ -1,13 +1,15 @@
 package com.whitewoodcity;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   @Override
   public void start(Stage stage) {
-    stage.setScene(new Scene(GameApp.embeddedLaunch(new GameApp())));
+    stage.setScene(new Scene(new Group(new Label("Hello World~!"))));
 
     stage.show();
   }
